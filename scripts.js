fa7e404233f5cede872d31e4e1a4ee812c019aaf
PabloD25 <pablo.davtorres@gmail.com>
2024-01-09 20:52:52 +0000

    2024-01-09 02:52pm

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 const slides = document.querySelector('.slides');
 const slideWidth = document.querySelector('.slide').clientWidth;
+const textOverlays = document.querySelectorAll('.text-overlay');
 
-let counter = 1;
+textOverlays.forEach(text => {
+  text.addEventListener('click', () => {
+    const slideIndex = parseInt(text.getAttribute('data-slide'));
+    slides.style.transition = 'transform 0.5s ease-in-out';
+    slides.style.transform = `translateX(${-slideWidth * slideIndex}px)`;
+  });
+});
+
+let counter = 0;
 
 setInterval(() => {
   slides.style.transition = 'transform 0.5s ease-in-out';
@@ -12,7 +21,7 @@ setInterval(() => {
     setTimeout(() => {
       slides.style.transition = 'none';
       slides.style.transform = 'translateX(0)';
-      counter = 1;
+      counter = 0;
     }, 500);
   }
 }, 3000); // Cambia la imagen cada 3 segundos (3000 milisegundos)
