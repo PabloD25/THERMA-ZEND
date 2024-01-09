a68dababed2072e1b020097d2bb57f7125c62ade
PabloD25 <pablo.davtorres@gmail.com>
2024-01-09 21:19:41 +0000

    2024-01-09 03:19pm

--- scripts.js
+++ scripts.js
@@ -1,60 +1,11 @@
-const slides = document.querySelector('.slides');
-const slideWidth = document.querySelector('.slide').clientWidth;
+const contenedor = document.getElementById('contenedor');
+const titulo1 = document.getElementById('titulo1');
+const titulo2 = document.getElementById('titulo2');
 
-// Datos de las imágenes y sus textos superpuestos
-const imagenesConTextos = [
-  {
-    imagenSrc: 'images/Imagen1.png',
-    imagenAlt: 'Descripción de la imagen 1',
-    textos: ['Texto 1', 'Texto 2', 'Texto 3']
-  },
-  {
-    imagenSrc: 'images/Imagen2.png',
-    imagenAlt: 'Descripción de la imagen 2',
-    textos: ['Texto A', 'Texto B', 'Texto C']
-  },
-  // Agrega más objetos para más imágenes
-];
-
-imagenesConTextos.forEach(imagenData => {
-  const slide = document.createElement('div');
-  slide.classList.add('slide');
-  const image = document.createElement('img');
-  image.src = imagenData.imagenSrc;
-  image.alt = imagenData.imagenAlt;
-  slide.appendChild(image);
-
-  const table = document.createElement('table');
-  table.classList.add('text-overlay-table');
-  const row = document.createElement('tr');
-
-  imagenData.textos.forEach((texto, index) => {
-    const cell = document.createElement('td');
-    const textOverlay = document.createElement('div');
-    textOverlay.classList.add('text-overlay');
-    textOverlay.setAttribute('data-slide', `${index}`);
-    textOverlay.textContent = texto;
-    cell.appendChild(textOverlay);
-    row.appendChild(cell);
-  });
-
-  table.appendChild(row);
-  slide.appendChild(table);
-  slides.appendChild(slide);
+titulo1.addEventListener('click', () => {
+  contenedor.style.backgroundImage = "url('images/Imagen1.png')"; // Reemplaza con la ruta de tu primera imagen
 });
 
-let counter = 0;
-
-setInterval(() => {
-  slides.style.transition = 'transform 0.5s ease-in-out';
-  slides.style.transform = `translateX(${-slideWidth * counter}px)`;
-  counter++;
-
-  if (counter === slides.children.length) {
-    setTimeout(() => {
-      slides.style.transition = 'none';
-      slides.style.transform = 'translateX(0)';
-      counter = 0;
-    }, 500);
-  }
-}, 3000);
+titulo2.addEventListener('click', () => {
+  contenedor.style.backgroundImage = "url('images/Imagen2.png')"; // Reemplaza con la ruta de tu segunda imagen
+});
