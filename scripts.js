7227f7705bc529eb65df8a83c6231ba0b21ef690
PabloD25 <pablo.davtorres@gmail.com>
2024-01-09 20:58:36 +0000

    2024-01-09 02:58pm

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
 const slides = document.querySelector('.slides');
 const slideWidth = document.querySelector('.slide').clientWidth;
-const textOverlays = document.querySelectorAll('.text-overlay');
 
-textOverlays.forEach(text => {
-  text.addEventListener('click', () => {
-    const slideIndex = parseInt(text.getAttribute('data-slide'));
-    slides.style.transition = 'transform 0.5s ease-in-out';
-    slides.style.transform = `translateX(${-slideWidth * slideIndex}px)`;
+// Datos de los textos superpuestos
+const textosPorImagen = [
+  ['Texto 1', 'Texto 2'], // Textos para la primera imagen
+  ['Texto 1', 'Texto 2']  // Textos para la segunda imagen y así sucesivamente
+  // Agrega más arrays para más imágenes
+];
+
+textosPorImagen.forEach(textos => {
+  const slide = document.createElement('div');
+  slide.classList.add('slide');
+  const image = document.createElement('img');
+  image.src = 'images/Imagen1.png'; // Reemplaza 'ruta_a_tu_imagen.jpg' por la ruta correspondiente
+  image.alt = 'Descripción de la imagen'; // Agrega una descripción adecuada
+  slide.appendChild(image);
+
+  const table = document.createElement('table');
+  table.classList.add('text-overlay-table');
+  const row = document.createElement('tr');
+
+  textos.forEach((texto, index) => {
+    const cell = document.createElement('td');
+    const textOverlay = document.createElement('div');
+    textOverlay.classList.add('text-overlay');
+    textOverlay.setAttribute('data-slide', `${index}`);
+    textOverlay.textContent = texto;
+    cell.appendChild(textOverlay);
+    row.appendChild(cell);
   });
+
+  table.appendChild(row);
+  slide.appendChild(table);
+  slides.appendChild(slide);
 });
 
 let counter = 0;
