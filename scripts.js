749b600941c9f6aac5a362c1e1bc3480b535c6e5
PabloD25 <pablo.davtorres@gmail.com>
2024-01-10 03:02:27 +0000

    2024-01-09 09:01pm Carrusel, con transformaciones en vez de animaciones

--- scripts.js
+++ scripts.js
@@ -2,31 +2,32 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Referencias
   const btnCarrusel = document.querySelectorAll('.btn_carrusel');
-  const imagen_Uno = document.querySelector('#img_carrusel_1');
-  const imagen_Dos = document.querySelector('#img_carrusel_2');
-  const imagen_Tres = document.querySelector('#img_carrusel_3');
+  const tablaCarrusel = document.querySelector('.Tabla_Img_Carrusel');
+
   
   // Funciones
-  function moverImagen(cantidad) {
-    // Obtenemos la posición actual de la imagen
-    let posicionActual = parseInt(imagen.style.left) || 0;
-
-    // Calculamos la nueva posición sumando la cantidad deseada
-    let nuevaPosicion = posicionActual + cantidad;
-
-    // Aplicamos la nueva posición a la imagen
-    imagen.style.left = nuevaPosicion + 'px';
+  function moverDerecha(item) {
+    item.classList.add('Transicion');
+    item.style.transform = 'translateX(100%)';
+  }
+  function moverIzquierda(item) {
+    item.classList.add('Transicion');
+    item.style.transform = 'translateX(-100%)';
   }
 
   var imagen_actual = 0;
 
   btnCarrusel.forEach((btn, index) => {
     btn.addEventListener('click', function() {
-      if (index === 1) {
-        imagen_Uno.style.animation = 'Entra_por_derecha 1s forwards';
+      if(index === 0 ){
+        moverDerecha(tablaCarrusel)
       }
+      else if(index === 2){
+        moverIzquierda(tablaCarrusel)
+      }
+
+
     });
   });
 
-  
 });
