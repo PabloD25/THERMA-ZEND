0ccc8124a4b870bd0d8ba943fd9f8a9f32e3b09c
PabloD25 <pablo.davtorres@gmail.com>
2024-01-10 02:38:43 +0000

    2024-01-09 08:38pm Proceso

--- scripts.js
+++ scripts.js
@@ -22,51 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   btnCarrusel.forEach((btn, index) => {
     btn.addEventListener('click', function() {
-
-
       if (index === 1) {
         imagen_Uno.style.animation = 'Entra_por_derecha 1s forwards';
       }
-      // Otras condiciones para otras transiciones
-      
-      /**
-      // De imagen 1 a imagen 2 o Imagen 3
-      if (imagen_actual == 0 && (index === 1 || index === 2)) {
-        imagen_Uno.style.animation = 'Sale_por_izquierda 1s forwards';
-      }
-
-        if (index === 1){
-          imagen_Dos
-        }
-        else{
-          imagen_Tres
-        }
-      }
-      */
-      /**
-      else if (index === 1 && ){
-
-      }
-      // De imagen 2 a imagen 1
-      else if(){
-
-      }
-      // De una imagen a si misma
-      else{
-        // No pasa nada
-      }
-
-  
-      imagenActual.classList.remove('imagen-actual');
-      imagenActual.classList.add('imagen-nueva');
-      imagenActual.style.animation = '';
-  
-      imagenNueva.classList.remove('imagen-nueva');
-      imagenNueva.classList.add('imagen-actual');
-      imagenNueva.style.animation = '';
-  
-      imagenes[index].classList.add('imagen-nueva');
-       */
     });
   });
+
+  
 });
