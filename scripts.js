87cfe9139c1b3a3c4c864dfc2c293d1c020c9540
PabloD25 <pablo.davtorres@gmail.com>
2024-01-15 23:45:14 +0000

    2024-01-15 05:45pm Trabajando: Carrusel 2

--- scripts.js
+++ scripts.js
@@ -29,4 +29,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }); 
   });
+
+  /* Carrusel_2 */
+  // Imagenes
+  const diccionario_Clientes = {
+    0: '<img src="images/imagen1.png" alt="Cliente 1">',
+    1: '<img src="images/imagen2.png" alt="Cliente 2">',
+    2: '<img src="images/imagen3.png" alt="Cliente 3">',
+    3: '<img src="images/imagen4.png" alt="Cliente 4">',
+    4: '<img src="images/imagen5.png" alt="Cliente 5">',
+    5: '<img src="images/imagen6.png" alt="Cliente 6">',
+    6: '<img src="images/imagen7.png" alt="Cliente 7">',
+    7: '<img src="images/imagen8.png" alt="Cliente 8">',
+    8: '<img src="images/imagen9.png" alt="Cliente 9">',
+    9: '<img src="images/imagen10.png" alt="Cliente 10">'
+  };
+
+  // Referencias
+  const btnSiguiente = document.getElementById('btn_Siguiente_Carrusel_2');
+  const btnAnterior = document.getElementById('btn_Anterior_Carrusel_2');
+  const celdaConImagen = document.getElementById('');
+  const celdaConImagen = document.getElementById('celdaConImagen');
+  const celdaConImagen = document.getElementById('celdaConImagen');
+  const celdaConImagen = document.getElementById('celdaConImagen');
+  const celdaConImagen = document.getElementById('celdaConImagen');
+
+
+
+
+
 });
