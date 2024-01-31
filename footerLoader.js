e3347919bae9752a9ee92e4e6801a99e3b40533b
PabloD25 <pablo.davtorres@gmail.com>
2024-01-31 21:18:15 +0000

    2024-01-31 03:18pm Pie de pagina casi Listo

--- footerLoader.js
+++ footerLoader.js
@@ -1,7 +1,7 @@
 function loadFooter() {
     console.log("Hola, Entrando en función");
     // Cargar el pie de página desde footer/footer.html
-    fetch('footer/footer.html')
+    fetch('../../footer.html')
         .then(response => response.text())
         .then(data => {
             // Agregar el contenido al final del cuerpo
