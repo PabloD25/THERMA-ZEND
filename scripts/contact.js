33bfb66d1e8069544c9f864a50b5a8650a4b6e08
PabloD25 <pablo.davtorres@gmail.com>
2024-04-01 17:53:14 +0000

    2024-04-01 11:53am footer Correo Listo

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,19 @@ function openWhatsApp() {
     var numeroTelefono = "5535535733";
     window.open("whatsapp://send?phone=" + numeroTelefono);
     }
+
+function enviarCorreo() {
+    // Dirección de correo electrónico
+    var correoDestino = "scinfrarroja@hotmail.com";
+    // Asunto del correo
+    var asunto = "Información de Maquinas";
+
+    // Codificar el asunto para que sea parte del enlace mailto
+    asunto = encodeURIComponent(asunto);
+
+    // Generar el enlace mailto con el correo de destino y el asunto
+    var enlaceMailto = "mailto:" + correoDestino + "?subject=" + asunto;
+
+    // Abrir el cliente de correo predeterminado con el enlace mailto
+    window.location.href = enlaceMailto;
+}
