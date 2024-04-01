95fb5f0f408164fd1a62d1f863ca10d119d8cc65
PabloD25 <pablo.davtorres@gmail.com>
2024-04-01 16:24:40 +0000

    2024-04-01 10:24am Carrusel Dobladoras

---
+++ carrusel.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  /* Carrusel */
+
+  // Referencias
+  const containerCarrusel = document.querySelector('.Container_img_Carrusel');
+  const imagenes = document.querySelectorAll('.img_carrusel');
+  let imagen_actual = 0;
+
+  // Función para cambiar la imagen del carrusel
+  function cambiarImagen() {
+      // Incrementar el índice de la imagen actual
+      imagen_actual++;
+      // Reiniciar el índice si excede la cantidad de imágenes
+      if (imagen_actual >= imagenes.length) {
+          imagen_actual = 0;
+      }
+      // Calcular la cantidad de movimiento
+      const cantidadMovimiento = imagen_actual * 25;
+      // Mover el carrusel
+      moverItem(containerCarrusel, 'izquierda', cantidadMovimiento);
+  }
+
+  // Función para mover el item
+  function moverItem(item, direccion, cantidad) {
+      const movimiento = direccion === 'izquierda' ? `translateX(-${cantidad}%)` : `translateX(-${cantidad}%)`;
+      item.classList.add('Transicion');
+      item.style.transform = movimiento;
+  }
+
+  // Iniciar el carrusel automáticamente cada 3 segundo
+  setInterval(cambiarImagen, 3000);
+});
