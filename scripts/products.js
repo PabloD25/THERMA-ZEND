68c02ffdd573ed337ec577b0338da8d90bb1a343
PabloD25 <pablo.davtorres@gmail.com>
2024-04-03 14:25:14 +0000

    2024-04-03 08:25am Paginas de Productos

--- scripts/products.js
+++
@@ -1,9 +0,0 @@
-function cargarProducto(productName) {
-    fetch('../data/products.json')
-    .then(response => response.json())
-    .then(data => {
-      const productoData = data[productName];
-      sessionStorage.setItem("producto", JSON.stringify(productoData));
-      window.location.href = productoData.enlace;
-    });
-}
